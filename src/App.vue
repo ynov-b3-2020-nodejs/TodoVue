cffdb7c02e0ab46316a304f81fe36413c5bbeff3
Fayaah <louise.baulan@gmail.com>
2019-11-05 14:37:38 +0100

    Add project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Task/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Task from './components/Task.vue';
+
+export default {  
+  name: 'app',
+  components: {
+    Task,
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="checkbox" id="task-checkbox">
+    <label for="task-checkbox"> {{ msg }}</label>
+    <i class="fas fa-times"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    msg: {
+      type: String,
+      default: 'Type...',
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+
+</style>
