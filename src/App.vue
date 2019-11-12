be49a44a479e25a05b2db0d19d321222ab99a65d
Fayaah <louise.baulan@gmail.com>
2019-11-12 11:46:31 +0100

    Useless

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1> ToDo List</h1>
-    <input id="create-task" type="text" v-model="inputTask" @keypress="addTask" class="m-2 p-1">
+    <div>
+      <input id="create-task" type="text" v-model="inputTask" @keypress="addTask" class="m-2 p-1">
+      <input type="radio" name="sortValue" id="done" value="done" @click="sortTask">
+      <label for="done"><i class="far fa-square"></i></label>
+      <input type="radio" name="sortValue" id="undone" value="undone" @click="sortTask">
+      <label for="undone"><i class="far fa-check-square"></i></label>
+      <input type="radio" name="sortValue" id="all" value="all" @click="sortTask">
+      <label for="all"><i class="fas fa-globe-africa"></i></label>
+    </div>
     <Task v-for="(task, index) in filterByDone" :key="task.id" :task="task" @input="onTaskChange"
           class="p-0" @erase="eraseTask(index)"/>
-
-    <input type="radio" name="sortValue" id="done" value="done" @click="sortTask">
-    <label for="done"><i class="far fa-square"></i></label>
-    <input type="radio" name="sortValue" id="undone" value="undone" @click="sortTask">
-    <label for="undone"><i class="far fa-check-square"></i></label>
-    <input type="radio" name="sortValue" id="all" value="all" @click="sortTask">
-    <label for="all"><i class="fas fa-globe-africa"></i></label>
   </div>
 </template>
 
@@ -113,9 +114,5 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
-
-  #create-task {
-
   }
 </style>
